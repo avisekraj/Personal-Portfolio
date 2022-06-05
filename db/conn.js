a86983edd8346d1fb713332da40cd4dfe62a6a0c
Abhishek <araj14250@gmail.com>
2022-06-05 23:41:09 +0530

    Portfol

--- db/conn.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/porfolio", { useNewUrlParser: true }).then(() => {
-    console.log("sucess")
-}).catch((error) => {
-    console.log(error);
-})
--- src/app.js
+++ src/app.js
@@ -2,10 +2,6 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const html = require("html");
-require("../db/conn");
-const Massage = require("../models/contact");
-
-
 // middleware static 
 
 const port = process.env.PORT || 3000;
@@ -22,26 +18,7 @@ app.set("view engine", "html")
 app.get("/", (req, res) => {
     res.render('/index.html');
 });
-app.get("/submit", (req, res) => {
-    res.render('/submit');
-});
 
-// create a new user in our database
-
-app.post("/contact", async(req, res) => {
-    try {
-        const sendmsg = new Massage({
-            name: req.body.name,
-            email: req.body.email,
-            number: req.body.number,
-            message: req.body.message
-        })
-        const getting = await sendmsg.save();
-        res.redirect("index.html");
-    } catch (error) {
-        res.status(400).send("your error", error);
-    }
-});
 app.listen(port, () => {
     console.log(`server is running at port no: ${port}`)
 });
