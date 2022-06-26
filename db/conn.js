8cd29c62c1b8a812988e1abb5bbb54c9626dd1b6
Abhishek <araj14250@gmail.com>
2022-06-26 18:36:39 +0530

    editing

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/porfolio", { useNewUrlParser: true }).then(() => {
+mongoose.connect("mongodb+srv://devil:devil@cluster0.k53q7t5.mongodb.net/portfolio?retryWrites=true&w=majority", { useNewUrlParser: true }).then(() => {
     console.log("sucess")
 }).catch((error) => {
     console.log(error);
